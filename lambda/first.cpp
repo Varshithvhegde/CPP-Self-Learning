3469e71f6f2949f8670e311ad4331bcb176e9ba5
Varshith Hegde <varshithvh@gmail.com>
2023-11-09 15:25:49 +0530

    Check

--- lambda/first.cpp
+++ lambda/first.cpp
@@ -21,7 +21,25 @@ int main()
     */
 
     //     Using Lambda
-    
+    // [](int x) {
+    //     if(x%2==0){
+    //         cout<<x<<" is even number"<<endl;
+    //     }
+    //     else{
+    //         cout<<x<<" is odd number"<<endl;
+    //     }
+    // }
+    int d = 7;
     vector<int> v{2, 3, 7, 14, 23};
-    for_each(v.begin(), v.end(), [](int x) {std::cout<<x<<"\n";});
+    for_each(v.begin(), v.end(), [&](int x)
+             {
+        if(x%d==0){
+            cout<<x<<" is divisible by "<<d<<endl;
+        }
+        else{
+            cout<<x<<" is not divisible by "<<d<<endl;
+        } 
+        d=10; });
+
+    // cout<<d<<endl;
 }
